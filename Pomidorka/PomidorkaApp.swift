17fee8b5199e7efb61de048704dc3d7d2da9d5b2
Daniil Krivonogov <113389092+metalWillHelpYou@users.noreply.github.com>
2024-07-17 20:17:51 +0200

    Initial Commit

---
+++ Pomidorka/PomidorkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomidorkaApp.swift
+//  Pomidorka
+//
+//  Created by metalWillHelpYou on 17.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct PomidorkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
